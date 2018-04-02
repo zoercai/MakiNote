0cfc838ad8b8a9e8e39c1a940cfd1db7427a4636
zoercai <zoercai@gmail.com>
2018-04-02 12:05:06 +1000

    Use React

--- background.js
+++
@@ -1,7 +0,0 @@
-chrome.browserAction.onClicked.addListener(function (activeTab) {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (arrayOfTabs) {
-        var activeTab = arrayOfTabs[0];
-        var activeTabId = activeTab.id;
-        chrome.tabs.executeScript(activeTabId, { file: "addNote.js" });
-    });
-});
